965a04b3cfe28487d3a97b4ec0ebfcda25cff53f
JessicaA-CodeCrew <ms.jessica.avarie@gmail.com>
2019-08-22 12:12:32 -0500

    CWREVWHILE

---
+++ cwrev.py
@@ -0,0 +1,25 @@
+# Problem 1:
+# Create a program that prints the user input until they enter 'q' to quit.
+
+# askUser = input("Enter a number: ")
+# while askUser != 'Q':
+#     askUser = input("More Info: ")
+# print(askUser)
+
+
+# Problem 3:
+# Create a program that takes user input in a while loop. If they enter 1, print 1. If they enter 2, print 2. If they enter 3 print 3. If they enter ‘q’ or 0 (your choice), quit. Else, print “ERROR”.
+#
+# askUser = input('Enter 1 or 2: ')
+# while askUser == 1:
+#     print("1")
+#     if askUser == 2:
+#         print("2")
+
+
+# Problem 4:
+# Create a program that takes the user input until they enter 'q'. You should store all of their input and separate the input with a comma. Once they enter 'q', print all of the comma separated words they entered.
+
+ask1 = input("Enter Input: ")
+while ask1 == input():
+    print( ask1, ",",ask1, ",", ask1)
---
+++ reviewcw.py
@@ -0,0 +1,6 @@
+# Create a program that prints the user input until they enter 'q' to quit.
+
+askUser = input('User input: ')
+while askUser
+    print(askUser)
+
